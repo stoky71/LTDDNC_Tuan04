7cc4bc7850eafed42a43edb01b9c07536a05d7f9
stoky71 <81395239+stoky71@users.noreply.github.com>
2022-09-21 22:45:19 +0700

    first view

--- App.js
+++ App.js
@@ -1,33 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 
 
 
 export default function App() {
   const [gia,SetGia] = React.useState('141.800 đ')
-  const [count, setCount] = React.useState('')
+  const [count, setCount] = React.useState(0)
+  const onPressAdd = () => setCount(prevCount => prevCount + 1);
+  const onPressRemove = () => setCount(prevCount => prevCount - 1);
 
   return (
     <View style={styles.container}>
       <View style={styles.view1}>
         <Image source={require("./images/book.png")} style={styles.book}/>
-        <View style={styles.viewText}>
-          <Text style={styles.textTitle}>Nguyên hàm tích phân và ứng dụng</Text>
-          <Text style={styles.textTitle}>Cung cấp bởi Tiki Trading</Text>
-          <Text style={styles.textGiaSale}>{gia}</Text>
-          <Text style={styles.textGiaCu}>{gia}</Text>
-          <View style={styles.viewButton}>
-            <Button title='+' style={styles.buttonAddRemove}/>
-            <Text></Text>
-            <Button title='-' style={styles.buttonAddRemove}/>
-            <Text>Mua sau</Text>
+          <View style={styles.viewText}>
+            <Text style={styles.textTitle}>Nguyên hàm tích phân và ứng dụng</Text>
+            <Text style={styles.textTitle}>Cung cấp bởi Tiki Trading</Text>
+            <Text style={styles.textGiaSale}>{gia}</Text>
+            <Text style={styles.textGiaCu}>{gia}</Text>
+            <View style={styles.viewButton}>
+              <TouchableOpacity title='+' style={styles.buttonAddRemove} onPress={onPressAdd}>
+                <Text>+</Text>
+              </TouchableOpacity>
+              <Text>{count}</Text>            
+              <TouchableOpacity title='-' style={styles.buttonAddRemove} onPress={onPressRemove}>
+                <Text >-</Text>
+              </TouchableOpacity>
+              <Text style={{color:'blue', paddingLeft: 100, fontWeight: "800"}}>Mua sau</Text>
+            </View>
           </View>
+      </View>
+      <View style={styles.view4}>
+          <Text style={{paddingTop:10, fontWeight: "800"}}>Mã giảm giá đã lưu</Text>
+          <Text style={{color: 'blue', fontWeight: "800"}}>Xem tại đây</Text>
         </View>
-      </View>
-      <View style={styles.view2}> 
- 
-      </View>
     </View>
   );
 }
@@ -51,7 +58,8 @@ const styles = StyleSheet.create({
     paddingTop:35,
     flexDirection: "row",
     flexWrap: "wrap",
-    alignItems: "flex-start"
+    alignItems: "flex-start",
+    height: 5
   },
   viewText:{
     fontSize:15,  
@@ -76,19 +84,29 @@ const styles = StyleSheet.create({
     
   },
   buttonAddRemove:{
-    width: 15,
-    height: 15
+    paddingLeft: 10,
+    width: 25,
+    height: 35,
+    color: 'gray',
   },
   viewButton:{
-    flex: 1
+    flexDirection: 'row',
+    paddingTop:10
   },
-  view2:{
+  viewButtonMGG:{
     flex:1,
+  },
+  buttonMGG:{
+    height:45,
+    width: 245
   },
   view3:{
     flex:1,
   },
   view4:{ 
     flex:1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start"
   },
 });
